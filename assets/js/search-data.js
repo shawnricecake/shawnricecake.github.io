95352111629d2a59ea028a0cf412bfad667a1ced
shawnricecake <39080134+shawnricecake@users.noreply.github.com>
2026-01-29 19:30:37 +0000

    Deploying to gh-pages from @ shawnricecake/shawnricecake.github.io@609d3171b4d320db4b9764c6566011d83ec80406 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,6 +55,9 @@ ninja.data = [{
           section: "News",},{id: "news-got-one-paper-accepted-in-aaai-2026-ai-for-social-impact-track",
           title: 'Got one paper accepted in AAAI 2026 AI for Social Impact Track.',
           description: "",
+          section: "News",},{id: "news-got-one-paper-accepted-in-iclr-2026",
+          title: 'Got one paper accepted in ICLR 2026.',
+          description: "",
           section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
